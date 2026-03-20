f4c7e282e699893f193ee5ec868e6ef12c24d04b
AZIZBEK <pubgmobile2006xxl@gmail.com>
2026-03-20 05:58:16 +0500

    boshladi

---
+++ main.js
@@ -0,0 +1,93 @@
+// // 1-mashq
+
+// let numbers = [10, 20, 30, 40,];
+// console.log(numbers.length);
+
+// // 2-mashq
+
+// let fruits = ["apple", "banan","orange"];
+// console.log(fruits[fruits.length -1]);
+
+// // 3-mashq
+
+// let cities = ["toshkent","samarqand"];
+// cities.push ("buxoro")
+// console.log(cities);
+
+// // 4-mashq
+
+// let names = ["Ali", 'Vali', 'Hasan'];
+// for (let i = 0; i < names.length; i++){
+//     console.log(names[i]);
+// }
+
+// // 5-mashq
+
+// let nums = [3, 9, 1, 12, 5,];
+// console.log(Math.max(...nums));
+
+
+// yangi masala
+
+// let nums = [3, 6, 8, 11, 14, 19];
+// for (let i = 0; i < nums .length; i++){
+//     if(nums [i] % 2 !==0){
+//         console.log(nums[i]);
+//     }
+// }
+
+// keyingi masala
+
+// let fruits = ['appple', 'banab', "orange"];
+// fruits.shift(0,1)
+// console.log(fruits);
+
+// keyingi masala
+
+// let cars = ["BMW", 'AUDI',];
+// cars.push ("KAWASAKI")
+// console.log(cars);
+
+// keyingi masala
+
+// let words = ['hello', 'word'];
+// words = ['hello'+'word']
+// console.log(words);
+
+// keyingi masala
+
+// let nums = [3, 5, 12, 8];
+// if (nums) {
+//     console.log("katta son bor 12");
+// } else {
+//     console.log("katta son yo'q");   
+// }
+
+// keyingi masala
+
+// let nums = [1, -9, 7, 5];
+// let nums1 = nums.some(n=> n > 0);
+// if (nums1) {
+//     console.log("false");
+// } else {
+//     console.log("true");
+// }
+
+// keyingi masala
+
+// let colors = ["red", 'gren', 'blue', "yellow"];
+// colors.splice(1,1)
+// console.log(colors);
+
+// keyingi masala
+
+// let nums = [5, 2, 9, 1];
+// nums.sort((a, b) => a - b);
+// console.log(nums);
+
+// keyingi masala
+
+// let a = [1, 2]
+// let b = [3, 4]
+// let c = a.concat(b)
+// console.log(c);
